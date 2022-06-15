a893b27afff22bea449d82cc760d84a9a3c9ef66
audriusxp <palm@inbox.lt>
2022-06-15 10:31:01 +0300

    nesugalvoju kas turi buti dauginamuoju, seka sukurta

--- script.js
+++ script.js
@@ -2,7 +2,17 @@ window.onload = function()
 {
 const testinis = [3,5,2,1];
 console.log(testinis);
-let suma = testinis[0]*testinis[1]*testinis[2]*testinis[3];
-console.log(suma);
+let sandauga = masyvoSandauga(testinis);
+console.log(sandauga);
 
+
+}
+function masyvoSandauga(testinis){
+    for (let i = 0; i < testinis.length; i++) {
+       
+        sandauga =  i * testinis.length ;
+        
+        
+    }
+    return sandauga
 }
